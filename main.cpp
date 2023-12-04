0d8ae768b300c2ff2d2237bf6f349837455d2da4
thomascolehewatt <thomashewatt@ufl.edu>
2023-12-04 12:16:11 -0500

    New calculateCompatibility and new menu functionality.

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 #include <chrono>
 using namespace std::chrono;
 using namespace std;
+
 void readDescription(istream& input, string& field) {
     char c;
     field.clear();
@@ -54,7 +55,6 @@ int partition(vector<FoodItem>& items, int low, int high) {
     swap(items[i + 1], items[high]);
     return (i + 1);
 }
-
 void quickSort(vector<FoodItem>& items, int low, int high) {
     if (low < high) {
         int pi = partition(items, low, high);
@@ -63,6 +63,7 @@ void quickSort(vector<FoodItem>& items, int low, int high) {
         quickSort(items, pi + 1, high);
     }
 }
+
 void merge(vector<FoodItem>& items, int left, int mid, int right)
 {
     int n1 = mid-left+1;
@@ -103,8 +104,6 @@ void merge(vector<FoodItem>& items, int left, int mid, int right)
          k++;
         }
      }
-
-
 void mergeSort(vector<FoodItem>& items, int left, int right)
 {
     if(left< right)
@@ -118,102 +117,148 @@ void mergeSort(vector<FoodItem>& items, int left, int right)
 
 int main()
 {
+    bool inUse = true;
+    while (inUse) {
 
 
-    UserPreferences userPrefs{};
+        UserPreferences userPrefs{};
 
-    cout << "Rank the following options based on what’s most important to you using 1-5 \n";
-    userPrefs.vitaminA = getPreferenceInput("Foods High in Vitamin A");
-    userPrefs.vitaminC = getPreferenceInput("Foods High in Vitamin C");
-    userPrefs.fiber = getPreferenceInput("Foods High in Fiber");
-    userPrefs.calcium = getPreferenceInput("Foods High in Calcium");
-    userPrefs.protein = getPreferenceInput("Foods High in Protein");
-    userPrefs.monosaturatedFat = getPreferenceInput("Foods High in Monosaturated Fats");
-    userPrefs.carbs = getPreferenceInput("Foods High in Carbs");
-    userPrefs.iron = getPreferenceInput("Foods High in Iron");
+        cout << "Rank the following options based on what’s most important to you using 1-5 \n";
+        userPrefs.vitaminA = getPreferenceInput("Foods High in Vitamin A");
+        userPrefs.vitaminC = getPreferenceInput("Foods High in Vitamin C");
+        userPrefs.fiber = getPreferenceInput("Foods High in Fiber");
+        userPrefs.calcium = getPreferenceInput("Foods High in Calcium");
+        userPrefs.protein = getPreferenceInput("Foods High in Protein");
+        userPrefs.monosaturatedFat = getPreferenceInput("Foods High in Monosaturated Fats");
+        userPrefs.carbs = getPreferenceInput("Foods High in Carbs");
+        userPrefs.iron = getPreferenceInput("Foods High in Iron");
 
-    userPrefs.cholesterol = getPreferenceInput("Foods Low in Cholesterol");
-    userPrefs.sugar = getPreferenceInput("Foods Low in Sugar");
-    userPrefs.sodium = getPreferenceInput("Foods Low in Sodium");
+        userPrefs.cholesterol = getPreferenceInput("Foods Low in Cholesterol");
+        userPrefs.sugar = getPreferenceInput("Foods Low in Sugar");
+        userPrefs.sodium = getPreferenceInput("Foods Low in Sodium");
 
-    vector<FoodItem> foodItems; //we are loading here boys
+        vector<FoodItem> foodItems; //we are loading here boys
 
-    ifstream inFile("food_data.csv");
-    string line;
+        ifstream inFile("food_data.csv");
+        string line;
 
-    // Skip the header line
-    getline(inFile, line);
+        // Skip the header line
+        getline(inFile, line);
 
-    while (getline(inFile, line)) {
-        istringstream iss(line);
-        FoodItem item;
+        while (getline(inFile, line)) {
+            istringstream iss(line);
+            FoodItem item;
 
-        // Use a temporary variable for the unused columns
-        string temp;
+            // Use a temporary variable for the unused columns
+            string temp;
 
-        // Assuming the CSV columns are ordered as in the file and are comma-separated
-        getline(iss, temp, ','); //
-        getline(iss, temp, ','); // Skip 'Category'
+            // Assuming the CSV columns are ordered as in the file and are comma-separated
+            getline(iss, temp, ','); //
+            getline(iss, temp, ','); // Skip 'Category'
 
-        //Read the description of the food, using "" to denote the beginning and end of the description
-        readDescription(iss, item.name);
+            //Read the description of the food, using "" to denote the beginning and end of the description
+            readDescription(iss, item.name);
 
-        iss >> item.cholesterol; iss.ignore();
-        iss >> item.vitaminA; iss.ignore();
-        iss >> item.vitaminC; iss.ignore();
-        iss >> item.fiber; iss.ignore();
-        iss >> item.calcium; iss.ignore();
-        iss >> item.protein; iss.ignore();
-        iss >> item.monosaturatedFat; iss.ignore();
-        iss >> item.sodium; iss.ignore();
-        iss >> item.iron; iss.ignore();
-        iss >> item.sugar; iss.ignore();
-        iss >> item.carbs; iss.ignore();
+            iss >> item.cholesterol;
+            iss.ignore();
+            iss >> item.vitaminA;
+            iss.ignore();
+            iss >> item.vitaminC;
+            iss.ignore();
+            iss >> item.fiber;
+            iss.ignore();
+            iss >> item.calcium;
+            iss.ignore();
+            iss >> item.protein;
+            iss.ignore();
+            iss >> item.monosaturatedFat;
+            iss.ignore();
+            iss >> item.sodium;
+            iss.ignore();
+            iss >> item.iron;
+            iss.ignore();
+            iss >> item.sugar;
+            iss.ignore();
+            iss >> item.carbs;
+            iss.ignore();
 
 
-        foodItems.push_back(item);
-    }
+            foodItems.push_back(item);
+        }
 
 ///*====================Conversion to Grams???====================*/
 //    for (auto& item : foodItems) {
 //        convertNutrientValuesToGrams(item);
 //    }
 
-    // Calculate compatibility for each food item
-    for (auto& item : foodItems) {
-        item.compatibility = calculateCompatibility(item, userPrefs);
-    }
-    auto mergeItems = foodItems;
-    auto startQuick = high_resolution_clock::now();
-    quickSort(foodItems, 0, foodItems.size());
-    auto stopQuick = high_resolution_clock::now();
-    auto durationQuick = duration_cast<microseconds>(stopQuick - startQuick);
-    cout << "\n" << "Quick Sort Time: " << durationQuick.count() << " microseconds" << endl;
+        // Calculate compatibility for each food item
+        for (auto &item: foodItems) {
+            item.compatibility = calculateCompatibility(item, userPrefs);
+        }
+        auto mergeItems = foodItems;
+        auto startQuick = high_resolution_clock::now();
+        quickSort(foodItems, 0, foodItems.size());
+        auto stopQuick = high_resolution_clock::now();
+        auto durationQuick = duration_cast<microseconds>(stopQuick - startQuick);
+        cout << "\n" << "Quick Sort Time: " << durationQuick.count() << " microseconds" << endl;
 
-    auto startMerge = high_resolution_clock::now();
-    mergeSort(mergeItems, 0, foodItems.size());
-    auto stopMerge = high_resolution_clock::now();
-    auto durationMerge = duration_cast<microseconds>(stopMerge - startMerge);
-    cout << "Merge Sort Time: " << durationMerge.count() << " microseconds" << "\n" << "\n";
+        auto startMerge = high_resolution_clock::now();
+        mergeSort(mergeItems, 0, foodItems.size());
+        auto stopMerge = high_resolution_clock::now();
+        auto durationMerge = duration_cast<microseconds>(stopMerge - startMerge);
+        cout << "Merge Sort Time: " << durationMerge.count() << " microseconds" << "\n" << "\n";
 
-    // Print the compatibility of each food item
-    int j = 1;
-    for (int i = foodItems.size(); i > foodItems.size()-10; i--) {
-        cout << j << ". " << foodItems[i].name << " - Compatibility: " << foodItems[i].compatibility << endl;
-        j++;
+        // Print the compatibility of each food item
+        int j = 1;
+        for (int i = foodItems.size(); i > foodItems.size() - 10; i--) {
+            cout << j << ". " << foodItems[i].name << " - Compatibility: " << foodItems[i].compatibility << endl;
+            j++;
+        }
+
+        bool choosing = true;
+        while (choosing) {
+            cout << endl;
+            cout << "Press 'r' to restart" << endl;
+            cout << "Press 'e' to end" << endl;
+            cout << "Or the rank of a food to its specific stats" << endl;
+            string userOption;
+            cin >> userOption;
+
+            if (userOption == "r") {
+                choosing = false;
+                cout << endl;
+            }
+            else if (userOption == "e") {
+                inUse = false;
+                break;
+            }
+            else if (isdigit(userOption[0])) {
+                int num = stoi(userOption);
+                if (num >= 1 && num <= 10) {
+                    int num = stoi(userOption) - 1;
+                    cout << "Name: " << foodItems[foodItems.size() - num].name << endl;
+                    cout << "Cholesterol       | " << "   " << foodItems[foodItems.size() - num].cholesterol << endl;
+                    cout << "Vitamin A         | " << "   " << foodItems[foodItems.size() - num].vitaminA << endl;
+                    cout << "Vitamin C         | " << "   " << foodItems[foodItems.size() - num].vitaminC << endl;
+                    cout << "Fiber             | " << "   " << foodItems[foodItems.size() - num].fiber << endl;
+                    cout << "Calcium           | " << "   " << foodItems[foodItems.size() - num].calcium << endl;
+                    cout << "Protein           | " << "   " << foodItems[foodItems.size() - num].protein << endl;
+                    cout << "MonosaturatedFat  | " << "   " << foodItems[foodItems.size() - num].monosaturatedFat << endl;
+                    cout << "Protein           | " << "   " << foodItems[foodItems.size() - num].protein << endl;
+                    cout << "Carbohydrates     | " << "   " << foodItems[foodItems.size() - num].carbs << endl;
+                    cout << "Sodium            | " << "   " << foodItems[foodItems.size() - num].sodium << endl;
+                    cout << "Iron              | " << "   " << foodItems[foodItems.size() - num].iron << endl;
+                    cout << "Sugar             | " << "   " << foodItems[foodItems.size() - num].sugar << endl;
+                    continue;
+                }
+                else {
+                    cout << "Please enter a number 1-10" << endl << endl;
+                    continue;
+                }
+            }
+            else {
+                cout << "Please enter a valid input" << endl << endl;
+            }
+        }
     }
 };
-
-
-
-// Sort foodItems based on compatibility
-// quickSort(foodItems, 0, foodItems.size() - 1); or mergeSort(foodItems, 0, foodItems.size() - 1);
-
-
-// float compatibility = calculateCompatibility({vitaminA, vitaminC, fiber, calcium, protien, fats, sodium, iron, cholestrol, userPreferences);
-
-
-//TODO: Set up object to store data from csv most preferably, an unordred_map // I used vector
-//Create function to determine how compatiable a food is based on user specifications //
-//Create a function to print out the foods that are compatiable with the user
-//Create a function to print out food to avoid
