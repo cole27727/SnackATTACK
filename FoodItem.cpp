c5f82abbfcdb39aac8b64e6eb9a2ca1c1d7f26c2
danicius <daniellealvarez00@gmail.com>
2023-12-04 11:06:36 -0500

    new compatibity function

--- FoodItem.cpp
+++ FoodItem.cpp
@@ -17,21 +17,92 @@ void convertNutrientValuesToGrams(FoodItem& item) {
 
 float calculateCompatibility(const FoodItem& item, const UserPreferences& prefs) {
     float score = 0.0f;
-    score += item.vitaminA * prefs.vitaminA;
-    score += item.vitaminC * prefs.vitaminC;
-    score += item.fiber * prefs.fiber;
-    score += item.calcium * prefs.calcium;
-    score += item.protein * prefs.protein;
-    score += item.monosaturatedFat * prefs.monosaturatedFat;
-    score += item.carbs * prefs.carbs;
-    score += item.iron * prefs.iron;
+    int count = 0;
 
-    score -= item.cholesterol * prefs.cholesterol;
-    score -= item.sodium * prefs.sodium;
-    score -= item.sugar * prefs.sugar;
+    // Function to calculate the score for each nutrient
+    auto calcScore = [](float nutrientValue, int preference, float standardValue) {
+        // Normalize preference to a percentage (20% to 100%)
+        float normPreference = preference * 20.0f;// 20% is the lowest preference
 
-    return score;
-}
+        // Calculate the proportion of the nutrient value to the standard value
+        float proportion = (standardValue > 0) ? (nutrientValue / standardValue) * 100.0f : 0.0f;
+
+        // Calculate the score based on how close the proportion is to the normalized preference
+        return 100.0f - std::abs(normPreference - proportion); // 100% is a perfect match
+    };
+
+    // Standard values for nutrients (example values, adjust according to a standard dietary reference)
+    const float standardVitaminA = 900; // mcg (micrograms)
+    const float standardVitaminC = 90; // mg   (milligrams)
+    const float standardCalcium = 1000; // mg
+    const float standardProtein = 50; // g (grams)
+    const float standardFiber = 25; // g
+    const float standardSodium = 2300; // mg
+    const float standardIron = 8; // mg
+    const float standardCholesterol = 300; // mg
+    const float standardSugar = 50; // g
+    const float standardCarbs = 300; // g
+    const float standardFats = 65; // g
+
+
+
+    // ... Define standard values for other nutrients like the ones above here
+
+    // Calculate score for each nutrient based on user preferences
+    if (prefs.vitaminA > 0) {
+        score += calcScore(item.vitaminA, prefs.vitaminA, standardVitaminA);
+        count++;
+    }
+    if (prefs.vitaminC > 0) {
+        score += calcScore(item.vitaminC, prefs.vitaminC, standardVitaminC);
+        count++;
+    }
+    if (prefs.fiber > 0) {
+        score += calcScore(item.fiber, prefs.fiber, standardFiber);
+        count++;
+    }
+    if (prefs.calcium > 0) {
+        score += calcScore(item.calcium, prefs.calcium, standardCalcium);
+        count++;
+    }
+    if (prefs.protein > 0) {
+        score += calcScore(item.protein, prefs.protein, standardProtein);
+        count++;
+    }
+    if (prefs.monosaturatedFat > 0) {
+        score += calcScore(item.monosaturatedFat, prefs.monosaturatedFat, standardFats);
+        count++;
+    }
+    if (prefs.carbs > 0) {
+        score += calcScore(item.carbs, prefs.carbs, standardCarbs);
+        count++;
+    }
+
+    if (prefs.iron > 0) {
+        score += calcScore(item.iron, prefs.iron, standardIron);
+        count++;
+    }
+    if (prefs.cholesterol > 0) {
+        score += calcScore(item.cholesterol, prefs.cholesterol, standardCholesterol);
+        count++;
+    }
+    if (prefs.sugar > 0) {
+        score += calcScore(item.sugar, prefs.sugar, standardSugar);
+        count++;
+    }
+    if (prefs.sodium > 0) {
+        score += calcScore(item.sodium, prefs.sodium, standardSodium);
+        count++;
+    }
+
+      // ... Calculate score for other nutrients here down below
+
+
+        // Normalize the score
+        return (count > 0) ? (score / count) : 0;
+    }
+
+
 
 void printNutrition(const std::vector<FoodItem>& foodItems, int preference){
     for (const auto& item : foodItems) {
